7c1319160ea047f5f41566713b16aa447d7828c7
Mohit Singh <ms9448815@gmail.com>
2025-09-06 07:28:37 +0530

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-auth.js";
-import { app } from "./app.js";
-
-// Initialize Firestore and Auth
-const db = getFirestore(app);
-const auth = getAuth(app);
-
-export { db, auth };
